f0610434fd4f176ca651a16a511b4fcbe3012e8c
LekalakalaSR <29257816@mylife.unisa.ac.za>
2026-04-29 13:47:01 +0200

    File renaming error fix

---
+++ Natural Angular Frequency.m
@@ -0,0 +1,13 @@
+% 1.1 - Natural Angular Frequency
+m = 5;          % mass in kg
+k = 2000;       % stiffness in N/m
+
+wn = sqrt(k/m);
+
+fprintf('Natural angular frequency wn = %.4f rad/s\n', wn);
+
+%[appendix]{"version":"1.0"}
+%---
+%[metadata:view]
+%   data: {"layout":"inline","rightPanelPercent":51.9}
+%---
